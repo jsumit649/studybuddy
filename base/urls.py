5445a5c958ce3e923e298dd275ebe9204a51e97d
Sumit Jha <jsumit649@gmail.com>
2025-06-18 22:48:42 +0530

    Room Form

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('room/<str:pk>/', views.room, name='room'),  # Room page
+
+    path('crete-room/', views.createRoom, name="create-room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
+from .models import Room
+
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Let's learn Python!"},
-    {'id': 2, 'name': 'Design with Figma'},
-    {'id': 3, 'name': 'Frontend Developers'},
-    {'id': 4, 'name': 'Backend Developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': "Let's learn Python!"},
+#     {'id': 2, 'name': 'Design with Figma'},
+#     {'id': 3, 'name': 'Frontend Developers'},
+#     {'id': 4, 'name': 'Backend Developers'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all
     context =  {"rooms": rooms}
     return render(request, 'base/home.html', context)  # Render the home page template
 
 def room(request, pk): 
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}  # Prepare context with the room data
 
     return render(request, 'base/room.html', context)  # Render the room page template
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
