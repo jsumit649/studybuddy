b23d8b646f9623ad4b7bb86c193c34743b23440b
Sumit Jha <jsumit649@gmail.com>
2025-06-20 15:45:39 +0530

    updated superuser issue

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+
     path('', views.home, name='home'),  # Home page
     path('room/<str:pk>/', views.room, name='room'),  # Room page
 
-    path('crete-room/', views.createRoom, name="create-room"),
+    path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room,Topic
 from .forms import RoomForm
 
@@ -12,6 +15,26 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend Developers'},
 #     {'id': 4, 'name': 'Backend Developers'},
 # ]
+def loginPage(request):
+
+    if request.method =='POST':
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages. error(request, "Username OR Password doesn't exist")
+        
+    context ={}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
